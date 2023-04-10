3eb05281a6ecebb8f1cc69248db46f67031a6a5a
LuisPOZO <luispozoengineer@gmail.com>
2023-04-09 19:39:25 -0600

    cambios en el modelo y el controlador

--- src/main/java/com/basedatos/basededatos/models/UserModel.java
+++ src/main/java/com/basedatos/basededatos/models/UserModel.java
@@ -2,18 +2,23 @@ package com.basedatos.basededatos.models;
 
 import jakarta.persistence.*;
 @Entity
-@Table(schema = "user")
+@Table(schema = "registrar")
 public class UserModel {
     @Id
     @Column(name = "id",updatable = false, nullable = false,  unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "nombre")
+    private String nombre;
 
-    @Column(name = "first_name")
-    private String firstName;
+    @Column(name = "usuario")
+    private String usuario;
 
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "contraseña")
+    private String contraseña;
 
     public long getId() {
         return id;
@@ -23,20 +28,35 @@ public class UserModel {
         this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
-    
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
